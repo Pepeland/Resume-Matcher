ede225b45e2fe7d133c5e2bf84c5ec902a282abe
Hamid <hamidprime@gmail.com>
2023-11-20 18:20:40 -0800

    add vercel analytics.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,6 +2,8 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { inject } from '@vercel/analytics'; 
+inject();
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes)]
